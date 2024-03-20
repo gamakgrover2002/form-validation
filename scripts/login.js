cf8c62af1666358a507f25396bda98d49d1b2ff0
Gamak Grover <112484328+gamakgrover2002@users.noreply.github.com>
2024-03-21 03:59:50 +0530

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,45 @@
 const OnSubmit = () => {
-  const password = document.getElementById("password").value;
-  const username = document.getElementById("username").value;
-  const specialCharacters = /[!@#$%^&*(),.?":{}|<>]/;
-  if (!username) {
-    alert("username missing");
-  } else if (!password) {
-    alert("password missing");
-  } else if (username.length < 6) {
-    alert("username too short");
-  } else if (password.length < 8) {
-    alert("password length too short");
-  } else if (!specialCharacters.test(password)) {
-    alert("password should include a special character");
+  const firstname = document.getElementById("firstname").value;
+  const lastname = document.getElementById("lastname").value;
+  const age = document.getElementById("age").value;
+  const email = document.getElementById("email").value;
+  const mobilenumber = document.getElementById("mobilenumber").value;
+  const alternatenumber = document.getElementById("alternate-number").value;
+
+  const currentaddress = document.getElementById("currentaddress").value;
+  if (!firstname) {
+    alert(" First name missing");
+  } else if (!lastname) {
+    alert("last name missing");
+  }
+  else if (!age) {
+    alert("age missing");
+  }
+  else if (!mobilenumber) {
+    alert("mobile number missing");
+  }
+  else if (!email) {
+    alert("email missing");
+  } else if (!currentaddress) {
+    alert("current address missing");
+  }
+
+  else if (age < 18) {
+    alert("minimum age should be 18");
+  } else if (mobilenumber.length != 10 || alternatenumber.length != 10) {
+    alert("invalid mobile number");
+  } else if (mobilenumber == alternatenumber) {
+    alert("alerternate number and contact number should not be same");
+  } else if (email.indexOf("@") < 0 || !email.includes(".com")) {
+    alert("email not valid");
   } else {
-    confirm("Login successful");
+    confirm("Details submitted successfully");
   }
 };
+function check() {
+  const radio = document.getElementById("radio").checked;
+  if (radio) {
+    document.getElementById("Permanentaddress").value =
+      document.getElementById("currentaddress").value;
+  }
+}
